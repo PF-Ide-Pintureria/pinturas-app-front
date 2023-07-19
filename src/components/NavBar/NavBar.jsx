09ae977fe1dd014a14413a84a9c2e2d3b8bae772
Carolina Garzon <carogrzn97@gmail.com>
2023-07-18 21:47:16 -0300

    [FE]adding style with tailwind

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,17 @@
 import { NavLink } from "react-router-dom";
-import style from "./NavBar.module.css";
 import React from 'react';
 import logo from '../../img/logoIde.png'
 
 const Nav = ()  => {
     return (
-        <div className={style.container}>
-            <NavLink to="/contact"> Contacto </NavLink>
-            <NavLink to="/about"> Sobre Nosotros </NavLink>
-            <NavLink to="/"> <img src={logo} alt="logo Ide Pinturerias" /></NavLink>
-            <NavLink to="/blog"> Blog </NavLink>
-            <NavLink to="/cart"> Mi carrito </NavLink>
-            <NavLink to="/account"> Mi cuenta </NavLink>
+        <div className='w-screen h-48 mb-6 flex justify-around items-center font-mono'>
+            <NavLink to="/contact" className="bg-yellow-400 bg-opacity-75 rounded-lg px-3 font-mono"> Pinturas </NavLink>
+            <NavLink to="/contact" className="font-mono"> Contacto </NavLink>
+            <NavLink to="/about" className="bg-yellow-400 bg-opacity-75 rounded-lg px-3 font-mono"> Sobre Nosotros </NavLink>
+            <NavLink to="/" > <img src={logo} alt="logo Ide Pinturerias" className="w-40 "/></NavLink>
+            <NavLink to="/blog" className="font-mono"> Blog </NavLink>
+            <NavLink to="/cart" className="font-mono"> Mi carrito </NavLink>
+            <NavLink to="/account" className="font-mono"> Mi cuenta </NavLink>
         </div>
     )
 }
