8a1c03e4572ed13a57f8add687a7743a36701327
jp.delafuente <jp.delafuente.va@hotmail.com>
2023-07-21 14:04:32 -0400

    añadiendo vista AdminDashboard en Account

---
+++ src/views/Account/Account.jsx
@@ -0,0 +1,13 @@
+import React from "react"; 
+import CreateForm from "../../components/CreateForm/CreateForm";
+import UpdateForm from "../../components/UpdateForm/UpdateForm";
+
+const Account = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Account;
