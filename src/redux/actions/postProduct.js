f765af6a2893873f12c2dda9f5253765ba8f4d99
jp.delafuente <jp.delafuente.va@hotmail.com>
2023-07-21 13:31:27 -0400

    Modificaciones en el CreateForm

--- src/redux/actions/postProduct.js
+++ src/redux/actions/postProduct.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { POST_PRODUCT } from '../action-type';
 
-export const postProduct = (form) => {
+export const postProduct = (productCreate) => {
     return async (dispatch) => {
-        const newProduct = (await axios.post("https://back-server-pinturas-app.onrender.com/products", form)).data;
+        const newProduct = (await axios.post("https://back-server-pinturas-app.onrender.com/products", productCreate)).data.products;
         dispatch({ type: POST_PRODUCT, payload: newProduct });
     }
 }
