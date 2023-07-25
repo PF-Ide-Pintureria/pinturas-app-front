1a94724168ddc81c4bac047f42c907db058f18ba
CBarreraB <124757365+CBarreraB@users.noreply.github.com>
2023-07-24 22:59:33 -0500

    All Proyect Folders

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,11 @@ import UpdateProduct from "./views/UpdateProduct/UpdateProduct";
 import CreateProduct from "./views/CreateProduct/CreateProduct";
 import Developers from "./views/Developers/Developers";
 import Location from "./views/Location/Location";
+import Profile from "./views/Profile/Profile";
+import Reviews from "./views/Reviews/Reviews";
+import Favorite from "./views/Profile/Favorite/Favorite";
+import Register from "./views/Register/Register";
+import Purchases from "./views/Purchases/Purchases";
 
 function App() {
   return (
@@ -25,18 +30,24 @@ function App() {
       </header>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:idProduct" element={<Detail />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/create" element={<CreateProduct />} />
-        <Route path="/blog" element={<Blog />} />
+        <Route path="/products/edit/:idProduct" element={<UpdateProduct />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/purchases" element={<Purchases />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/favorite" element={<Favorite />} />
         <Route path="/account" element={<AdminDashboard />} />
         <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:idProduct" element={<Detail />} />
-        <Route path="/products/edit/:idProduct" element={<UpdateProduct />} />
-        <Route path="/developers" element={<Developers />} />
         <Route path="/location" element={<Location />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/developers" element={<Developers />} />
       </Routes>
       <footer>
         <Footer />
