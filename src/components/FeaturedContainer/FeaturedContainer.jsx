71ff20691325414ceba61ab37dccf5fb0ec5aa73
CBarreraB <124757365+CBarreraB@users.noreply.github.com>
2023-07-24 21:37:52 -0500

    Delete Featured

--- src/components/FeaturedContainer/FeaturedContainer.jsx
+++ src/components/FeaturedContainer/FeaturedContainer.jsx
@@ -1,19 +1,18 @@
-import Featured from "../Featured/Featured";
+import Products from "../Products/Products";
 import featuredBanner from "../../img/featured-banner.png";
 import React from "react";
 import { useSelector } from "react-redux";
 
 const FeaturedContainer = () => {
   const bestSellers = useSelector((state) => state.bestSell);
-  
+
   return (
     <div className="flex  w-full m-auto">
       <div className="flex flex-col justify-center m-0 w-full">
-        
         <img src={featuredBanner} alt="banner" className="w-full mt-20" />
         <div className="flex w-4/5 column-4 gap-5 my-10 self-center justify-around object-center">
           {bestSellers.map((bestSeller) => (
-            <Featured
+            <Products
               key={bestSeller.idProduct}
               id={bestSeller.idProduct}
               image={bestSeller.image}
