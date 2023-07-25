81b5735c3400663d28f95474388c89224d539fc0
CBarreraB <124757365+CBarreraB@users.noreply.github.com>
2023-07-25 12:32:40 -0500

    Add Route Admin

---
+++ src/views/Admin/AdminDashboard/AdminDashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CreateForm from "../../../components/CreateForm/CreateForm";
+import UpdateForm from "../../../components/UpdateForm/UpdateForm";
+import NavbarAdmin from "../../../components/NavbarAdmin/NavbarAdmin";
+
+const AdmindDashboard = () => {
+  return (
+    <div>
+      <div className="background">
+        <NavbarAdmin />
+      </div>
+    </div>
+  );
+};
+
+export default AdmindDashboard;
