826bff52cdd45190249b4aa09b068047f79b34ff
jp.delafuente <jp.delafuente.va@hotmail.com>
2023-07-28 16:36:42 -0400

    HOTFIX postTegisterUser Action

--- src/redux/actions/postRegisterUser.js
+++ src/redux/actions/postRegisterUser.js
@@ -4,8 +4,7 @@ import { POST_REGISTER_USER, BASE_URL } from '../action-type';
 export const postRegisterUser = (userCreate) => {
     return async (dispatch) => {
         try {
-            //   const response = await axios.post(`${BASE_URL}users/register/`, userCreate);
-            const response = await axios.post(`https://back-server-pinturas-app-pr-53.onrender.com/users/register/`, userCreate);
+            const response = await axios.post(`${BASE_URL}users/register/`, userCreate);
 
             const newUser = response.status === 'success' ? response.user : {};
             dispatch({ type: POST_REGISTER_USER, payload: newUser });
