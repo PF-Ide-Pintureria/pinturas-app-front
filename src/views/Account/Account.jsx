5d53a3553d8b1fa055b72930d83d5fdb17a3dbef
CBarreraB <124757365+CBarreraB@users.noreply.github.com>
2023-07-28 01:15:42 -0500

    Sidebar

--- src/views/Account/Account.jsx
+++ src/views/Account/Account.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import SideBar from "../../components/Account/Sidebar";
 import LoadingScreen from "../../components/Account/LoadingScreen";
-//import Dashboard from "../../components/Account/Dashboard";
+import Dashboard from "../../components/Account/Dashboard";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -13,7 +13,6 @@ import Orders from "../../components/Account/Orders";
 
 import LoginForm from "../../components/LoginForm/LoginForm";
 
-
 const Account = () => {
   const { isAuthenticated, user, logout, isLoading } = useAuth0();
   const [activeButton, setActiveButton] = useState("dashboard");
@@ -24,33 +23,52 @@ const Account = () => {
     setActiveButton(buttonName);
   };
 
-        return (
-            <div>
-                { !loggedUser 
-                ? (
-                <div style={{ display: "flex", minHeight: "100vh" }}>
-                    <SideBar
-                        isAuthenticated={isAuthenticated}
-                        user={user}
-                        activeButton={activeButton}
-                        handleButtonClick={handleButtonClick}
-                        logout={logout}
-                    />
-                    <div className="w-9/12" style={{ flex: "1" }}>
-                        <LoadingScreen isLoading={isLoading} />
-                        <Dashboard isAuthenticated={isAuthenticated} />
-                        <footer style={{ textAlign: "center", padding: "10px" }}></footer>
-                    </div>
-                </div>
-                ) : (
-                    <div className="pb-36 pt-20 my-20 flex items-center justify-center lg:mt-0 mr:auto h-full" >
-                        <LoginForm />
-                    </div>
-                )
-                }
-            </div>
-        );
-}
+  let activeComponent = null;
 
+  switch (activeButton) {
+    case "dashboard":
+      activeComponent = <Dashboard isAuthenticated={isAuthenticated} />;
+      break;
+    case "updateUserForm":
+      activeComponent = <UpdateUserForm />;
+      break;
+    case "addresses":
+      activeComponent = <Addresses />;
+      break;
+    case "favorities":
+      activeComponent = <Favorities />;
+      break;
+    case "orders":
+      activeComponent = <Orders />;
+      break;
+    default:
+      activeComponent = null;
+  }
+  return (
+    <div>
+      {!loggedUser ? (
+        <div style={{ display: "flex", minHeight: "100vh" }}>
+          <SideBar
+            isAuthenticated={isAuthenticated}
+            user={user}
+            activeButton={activeButton}
+            handleButtonClick={handleButtonClick}
+            logout={logout}
+          />
+          <div className="w-9/12" style={{ flex: "1" }}>
+            <LoadingScreen isLoading={isLoading} />
+            {activeComponent}
+            <Dashboard isAuthenticated={isAuthenticated} />
+            <footer style={{ textAlign: "center", padding: "10px" }}></footer>
+          </div>
+        </div>
+      ) : (
+        <div className="pb-36 pt-20 my-20 flex items-center justify-center lg:mt-0 mr:auto h-full">
+          <LoginForm />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Account;
