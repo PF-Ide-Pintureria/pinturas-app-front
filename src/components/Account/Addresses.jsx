ed9045d28fc26bb1805f9e84b5e89a363710eebb
CBarreraB <124757365+CBarreraB@users.noreply.github.com>
2023-07-28 09:09:08 -0500

    Models Dashboard

--- src/components/Account/Addresses.jsx
+++ src/components/Account/Addresses.jsx
@@ -3,36 +3,57 @@ import React from "react";
 const Addresses = () => {
   return (
     <div className="container mx-auto px-4">
-      este es Addresses
-      {/* <form className="w-full max-w-md" onSubmit={handleSubmit}>
+      <li>
+        <p className="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium focus:bg-gray-200 focus:shadow-outline">
+          <span className=" text-gray-600">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M8 21H12"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M10 21V3"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M10 4L19 8L10 12"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </span>
+          <span>Direcciones</span>
+        </p>
+      </li>
+      <form className="w-full max-w-md">
         <div className="mb-6">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-first-name">
-            Dirección
-          </label>
+          <p className="text-gray-600 text-xs mt-1">Provincia </p>
           <input
             className="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
-            id="grid-first-name"
-            type="text"
-            placeholder="Nombre"
-            value={name}
-            onChange={handleNameChange}
+            placeholder="Mariano Moreno 206"
+          />
+          <p className="text-gray-600 text-xs mt-1">Localidad </p>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+            placeholder="Villa Maria"
           />
           <p className="text-gray-600 text-xs mt-1">Provincia </p>
-        </div>
-
-        <div className="mb-6">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-email">
-            Actualiza dirección
-          </label>
           <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            placeholder="Nueva dirección"
-            value={email}
-            onChange={handleEmailChange}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+            placeholder="Cordoba"
           />
         </div>
 
@@ -49,7 +70,7 @@ const Addresses = () => {
           </button>
         </div>
       </form>
-      <footer style={{ textAlign: "center", padding: "14.5px" }}></footer> */}
+      <footer style={{ textAlign: "center", padding: "14.5px" }}></footer>
     </div>
   );
 };
--- src/components/Account/Orders.jsx
+++ src/components/Account/Orders.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Orders = () => {
-  return <div className="container mx-auto px-4">este es Orders</div>;
+  return (
+    <div className="container mx-auto px-4">
+      <li>
+        <p className="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium focus:bg-gray-200 focus:shadow-outline">
+          <span className="text-gray-600">
+            <svg
+              className="h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
+              />
+            </svg>
+          </span>
+          <span>Mis Pedidos</span>
+        </p>
+      </li>
+      <div className="content flex-1 min-h-[500px] overflow-y-auto p-4 bg-gray-200">
+        <p className="flex items-center space-x-3 text-gray-500 p-2 ">
+          No tienes pedidos activos
+        </p>
+      </div>
+      <div className="flex justify-between m-10">
+        <Link to="/products">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Ir a buscar Productos
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
