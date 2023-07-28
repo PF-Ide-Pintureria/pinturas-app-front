5b3eab77ede036fc98cd6af949d103c561605336
jp.delafuente <jp.delafuente.va@hotmail.com>
2023-07-27 22:12:59 -0400

    Creacion de vista login y ruta

---
+++ src/views/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginForm from "../../components/LoginForm/LoginForm";
+
+const Login = () => {
+    return (
+        <div>
+            <LoginForm />
+        </div>);
+}
+
+export default Login;
