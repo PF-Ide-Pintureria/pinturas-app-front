4fcb333b1f490d14d09f838b1cd17481b59acc72
cgnicole <soficamargo2004@gmail.com>
2023-07-29 13:10:03 -0500

    informacion usuarios auth0 guardada en una cookie

--- src/redux/action-type.js
+++ src/redux/action-type.js
@@ -1,25 +1,24 @@
 //PRODUCTS
-export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
-export const DELETE_PRODUCT = 'DELETE_PRODUCT';
-export const POST_PRODUCT = 'POST_PRODUCT';
-export const PUT_PRODUCT = 'PUT_PRODUCT';
-export const GET_PRODUCT_BY_NAME = 'GET_PRODUCT_BY_NAME';
-export const GET_PRODUCT_BY_ID = 'GET_PRODUCT_BY_ID';
+export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
+export const DELETE_PRODUCT = "DELETE_PRODUCT";
+export const POST_PRODUCT = "POST_PRODUCT";
+export const PUT_PRODUCT = "PUT_PRODUCT";
+export const GET_PRODUCT_BY_NAME = "GET_PRODUCT_BY_NAME";
+export const GET_PRODUCT_BY_ID = "GET_PRODUCT_BY_ID";
 
 //CATEGORIES
-export const GET_ALL_CATEGORIES = 'GET_ALL_CATEGORIES';
+export const GET_ALL_CATEGORIES = "GET_ALL_CATEGORIES";
 
 //USER
-export const GET_ALL_USERS = 'GET_ALL_USERS';
-export const POST_LOGIN_USER = 'POST_LOGIN_USER';
-export const POST_REGISTER_USER = 'POST_REGISTER_USER';
-export const PUT_USER = 'PUT_USER';
-export const DELETE_USER = 'DELETE_USER';
-export const LOGOUT_USER = 'LOGOUT_USER';
-
+export const GET_ALL_USERS = "GET_ALL_USERS";
+export const POST_LOGIN_USER = "POST_LOGIN_USER";
+export const POST_REGISTER_USER = "POST_REGISTER_USER";
+export const PUT_USER = "PUT_USER";
+export const DELETE_USER = "DELETE_USER";
+export const LOGOUT_USER = "LOGOUT_USER";
 
 //FILTERS
-export const GET_PRODUCT_FILTER = 'GET_PRODUCT_FILTER';
+export const GET_PRODUCT_FILTER = "GET_PRODUCT_FILTER";
 export const GET_BEST_SELL = "GET_BEST_SELL";
 export const SET_CATEGORY = "SET_CATEGORY";
 export const SET_HIGH_PRICE = "SET_HIGH_PRICE";
@@ -36,6 +35,11 @@ export const POST_CONTACT_EMAIL = "POST_CONTACT_EMAIL";
 export const POST_REGISTER_EMAIL = "POST_REGISTER_EMAIL";
 export const POST_ORDER_EMAIL = "POST_ORDER_EMAIL";
 
-
 //URL
 export const BASE_URL = "https://back-server-pinturas-app.onrender.com/";
+
+//AUTH0-USERS-INFO
+
+export const SET_USER_DATA = "SET_USER_DATA";
+
+export const URL_INFO_USER_AUTH0 = "ll";
