683e6ba6729229c3628532704e84d060c89a931d
jp.delafuente <jp.delafuente.va@hotmail.com>
2023-07-29 18:35:03 -0400

    Cambios en el login

--- src/redux/actions/postLoginUser.js
+++ src/redux/actions/postLoginUser.js
@@ -5,6 +5,7 @@ export const postLoginUser = (userLogin) => {
     return async (dispatch) => {
         try {
             const response = (await axios.post(`${BASE_URL}users/login`, userLogin)).data;
+            console.log('respuesta en el action: ', response)
             if (response?.acceso?.user?.active) {
                 const loginUser = response.acceso.user;
                 dispatch({ type: POST_LOGIN_USER, payload: loginUser });
