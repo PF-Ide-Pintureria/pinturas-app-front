02da16d7aef0f37a62b955fa46989c07178e34ef
cgnicole <soficamargo2004@gmail.com>
2023-07-28 19:34:53 -0500

    boton iniciar sesion con google

--- src/components/Account/Dashboard.jsx
+++ src/components/Account/Dashboard.jsx
@@ -56,7 +56,8 @@ const Dashboard = () => {
         <div style={containerStyle}>
           <div
             className="max-w-sm border-gray-200 rounded-lg shadow dark:bg-purple-700 dark:border-gray-700"
-            style={cardStyle}>
+            style={cardStyle}
+          >
             <a href="#">
               <img className="rounded-t-lg" src={login} alt="" />
             </a>
@@ -73,7 +74,8 @@ const Dashboard = () => {
               </p>
               <a
                 href="#"
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
                 <button onClick={() => loginWithRedirect()}>
                   Iniciar Sesión
                 </button>
@@ -82,7 +84,8 @@ const Dashboard = () => {
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
-                  viewBox="0 0 14 10">
+                  viewBox="0 0 14 10"
+                >
                   <path
                     stroke="currentColor"
                     strokeLinecap="round"
