6cd5558f6ebe52ae63f76dcc9b8faca6e648c406
Carolina Garzon <carogrzn97@gmail.com>
2023-07-31 21:33:38 -0300

    cart to nico

--- src/components/ProductCart/ProductCart.jsx
+++ src/components/ProductCart/ProductCart.jsx
@@ -8,29 +8,28 @@ const ProductCart = ({name, quantity, image, price, stock}) => {
   }
 
   return (
-    <div className=" py-3 my-5">
+    <div className=" py-3 my-5 border-t">
       
-      <div className="w-full grid grid-cols-3 gap-10">
-        <div className="grid grid-cols-2">
+      <div className="w-full grid grid-cols-2 gap-10">
+        <div className="flex">
           <div >
             <img src={image} alt="" className="w-20"/>
           </div>
-          <div className="w-32 flex items-start flex-col">
-            <div className="w-60">
-              <h1>{name}</h1>
-            </div>
-            <div >
+          <div className="flex px-5 flex-col">
+            <div className="">
+              <h1 className="text-base font-semibold">{name}</h1>
               <h1> stock: {stock}</h1>
             </div>
           </div>
         </div>
+        <div className="grid grid-cols-2">
+          <div className="flex justify-end items-center">
+            <h1 className="px-10">{quantity}</h1>
+          </div>
 
-        <div className="flex justify-end items-center">
-          <h1>{quantity}</h1>
-        </div>
-
-        <div className="flex justify-end items-center">
-          <h1>{calcPrice(quantity, price)}</h1>
+          <div className="flex justify-end items-center">
+            <h1>{calcPrice(quantity, price)}</h1>
+          </div>
         </div>
       </div>
     </div>
--- src/redux/actions/postCart.js
+++ src/redux/actions/postCart.js
@@ -3,15 +3,10 @@ import { POST_CART, BASE_URL } from '../action-type';
 import { useSelector } from "react-redux";
 
 export const postCart = (cart) => {
-    const user = useSelector((state) => state.user);
-    const cartToPost = {
-        idUser: user.id,
-        products: cart
-    }
 
     return async (dispatch) => {
         try {
-            const response = await axios.post(`${BASE_URL}/carts`, cartToPost);
+            const response = await axios.post(`${BASE_URL}/carts`, cart);
     
             const newCart =  response.data;
             dispatch({ type: POST_CART, payload: newCart });
