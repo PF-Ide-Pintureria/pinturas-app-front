eef213a7a5b8525dc413f8e52f006aeec81eb384
Carolina Garzon <carogrzn97@gmail.com>
2023-08-04 15:10:15 -0300

    got to pay

--- src/views/OrderDetail/OrderDetail.jsx
+++ src/views/OrderDetail/OrderDetail.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 const OrderDetail = () => {
+    const initPoint = useSelector((state) => state.initPoint);
     return (
-        <div> 
-            <a></a>
+        <div className="flex items-center justify-center m-20"> 
+            <a href={`${initPoint}`} className="inline-flex items-center justify-center rounded-md border-2 border-transparent bg-purple-800 bg-none px-12 py-3 text-center text-base font-bold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-gray-800">
+                Ir a Pagar
+            </a>
         </div>
     )
 }
