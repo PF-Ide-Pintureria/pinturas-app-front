fbed9204e7d6a8ed5723dba88a39e9bf0a4df1db
CBarreraB <124757365+CBarreraB@users.noreply.github.com>
2023-08-07 12:33:48 -0500

    Blog
    Estilos de Blog

--- src/views/Blog/Blog.jsx
+++ src/views/Blog/Blog.jsx
@@ -1,48 +1,48 @@
 import React, { useEffect } from "react";
-import style from "./Blog.module.css";
+import BlogBackground from "../../img/gradientBackground.png";
 import img from "../../img/blog.jpg";
 import BlogCard from "../../components/BlogCard/BlogCard";
 import { useDispatch, useSelector } from "react-redux";
 import getPosts from "../../redux/actions/Blog/getPosts";
 
 const Blog = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  useEffect(() => {
+    getPosts()(dispatch);
+  }, [dispatch]);
 
-    useEffect(() => {
-        getPosts()(dispatch);
-    }, [dispatch]);
+  const posts = useSelector((state) => state.posts);
 
-    const posts = useSelector(state => state.posts)
-
-    return (
-        <div className={style.container}>
-            <div className={style.background}>
-                <h1 className="text-3xl font-bold flex items-center justify-center">
-                    BLOG
-                </h1>
-                <div className="my-10 flex justify-center items-center">
-                    <div className="h-full w-full grid grid-cols-3 gap-3">
-
-                        {posts.map(post => {
-                            return (
-                                <BlogCard
-                                    key={post.idBlog}
-                                    id={post.idBlog}
-                                    image={post.image ? post.image : img}
-                                    title={post.title}
-                                    date={(Date(post.createdAt)).slice(0, 15)}
-                                    description={post.description}
-                                    author={post.userId}
-                                />
-
-                            )
-                        })}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="bg-primary min-h-screen">
+      <div
+        className="bg-cover bg-center"
+        style={{ backgroundImage: `url(${BlogBackground})` }}
+      >
+        <h1 className="text-3xl font-bold flex items-center justify-center">
+          BLOG
+        </h1>
+        <div className="my-10 flex justify-center items-center">
+          <div className="h-full w-full grid grid-cols-3 gap-3">
+            {posts.map((post) => {
+              return (
+                <BlogCard
+                  key={post.idBlog}
+                  id={post.idBlog}
+                  image={post.image ? post.image : img}
+                  title={post.title}
+                  date={Date(post.createdAt).slice(0, 15)}
+                  description={post.description}
+                  author={post.userId}
+                />
+              );
+            })}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/views/Blog/BlogDetail.jsx
+++ src/views/Blog/BlogDetail.jsx
@@ -2,21 +2,37 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import getPostById from "../../redux/actions/Blog/getPostById";
 import { useParams } from "react-router-dom";
+import BlogBackground from "../../img/gradientBackground.png";
 
 const BlogDetail = () => {
-    const dispatch = useDispatch();
-    const post = useSelector(state => state.post);
-    const { idBlog } = useParams()
-    useEffect(() => {
-        getPostById(idBlog)(dispatch);
-    }, [dispatch]);
+  const dispatch = useDispatch();
+  const post = useSelector((state) => state.post);
+  const { idBlog } = useParams();
 
+  useEffect(() => {
+    getPostById(idBlog)(dispatch);
+  }, [dispatch]);
 
-    return (
-        <div>
-
+  return (
+    <div className="bg-primary min-h-screen">
+      <div
+        className="bg-cover bg-center"
+        style={{ backgroundImage: `url(${BlogBackground})` }}
+      >
+        <div className="container mx-auto py-10 text-white">
+          <h1 className="text-4xl font-bold text-center my-6">{post.title}</h1>
+          <div className="flex justify-center">
+            <img
+              src={post.image ? post.image : BlogBackground}
+              alt="Blog"
+              className="w-full max-w-[400px] md:max-w-[600px] lg:max-w-[800px] xl:max-w-[1000px] h-auto object-cover rounded-md"
+            />
+          </div>
+          <div className="my-6">{post.description}</div>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default BlogDetail;
