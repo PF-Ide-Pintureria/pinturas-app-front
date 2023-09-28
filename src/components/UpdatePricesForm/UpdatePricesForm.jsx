faea8080e08b4b9be049df4d3a8f6ed90fec6ecb
Edgar Juan Guercio <jguercio@live.com.ar>
2023-09-28 20:00:56 -0300

    vista y funcionalidad formulario actualizar precios

--- src/components/UpdatePricesForm/UpdatePricesForm.jsx
+++ src/components/UpdatePricesForm/UpdatePricesForm.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-// import { allCategories } from '../../redux/actions/Categories/allCategories'
-// import validations from './validations'
-// import { formatAndPost } from './formatAndPost'
 import { getProviders } from '../../redux/actions/Providers/getProviders'
+import { formatAndPut } from './formatAndPut'
+// import validations from './validations'
 // import { useNavigate } from 'react-router-dom'
 // import Swal from 'sweetalert2'
 
@@ -11,23 +10,47 @@ const UpdatePricesForm = () => {
   const dispatch = useDispatch()
   const providers = useSelector(state => state.providers)
 
+  const [inputsForm, setInputsForm] = useState({
+    provider: '',
+    excelFile: ''
+  })
+
   useEffect(() => {
     dispatch(getProviders())
   }, [dispatch])
 
+  // Manejar estado local
+  const handleInputChange = (event) => {
+    const property = event.target.name
+    const value = event.target.value
+
+    if (property === 'excelFile') {
+      setInputsForm({
+        ...inputsForm,
+        excelFile: event.target.files[0]
+      })
+    } else {
+      setInputsForm({ ...inputsForm, [property]: value })
+    }
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    await formatAndPut(inputsForm)
+  }
   return (
-    <div className="p-4">
-      <h2 className="text-2xl font-semibold mb-4">Formulario</h2>
-      <form>
+    <div className="p-4 ">
+      <h2 className="text-2xl font-semibold mb-4">Actualizar Precios</h2>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
         <div className="mb-4">
           <label htmlFor="provider" className="block text-gray-700 text-sm font-bold mb-2">
             Selecciona un Proveedor
           </label>
           <select
                   className="bg-formBg rounded-r-lg w-72 h-8"
-                  // value={inputsForm.patent}
-                  // onChange={handleInputChange}
-                  name="patent"
+                  value={inputsForm.patent}
+                  onChange={handleInputChange}
+                  name="provider"
               >
                   <option style={{ textAlign: 'center' }} value="">
                       Selecciona una marca
@@ -40,16 +63,16 @@ const UpdatePricesForm = () => {
               </select>
         </div>
         <div className="mb-4">
-          <label htmlFor="priceList" className="block text-gray-700 text-sm font-bold mb-2">
+          <label htmlFor="excelFile" className="block text-gray-700 text-sm font-bold mb-2">
             Cargar Lista de Precios (Excel)
           </label>
           <input
             type="file"
             accept=".xlsx, .xls"
             className="w-full"
-            id="priceList"
-            name="priceList"
-            // onChange={handleFileUpload}
+            id="excelFile"
+            name="excelFile"
+            onChange={handleInputChange}
           />
         </div>
         <div className="text-center">
