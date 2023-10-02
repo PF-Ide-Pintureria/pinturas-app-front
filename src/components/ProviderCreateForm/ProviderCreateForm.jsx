09a50cbc15affcfd6a03b272ee609139c5a5cc92
Edgar Juan Guercio <jguercio@live.com.ar>
2023-10-01 22:46:25 -0300

    agregando validaciones

--- src/components/ProviderCreateForm/ProviderCreateForm.jsx
+++ src/components/ProviderCreateForm/ProviderCreateForm.jsx
@@ -17,7 +17,8 @@ const CreateProvider = () => {
   const [errors, setErrors] = useState({
     name: '',
     discount: '',
-    markup: ''
+    markup: '',
+    empty: ''
   })
 
   const handleChange = (event) => {
@@ -41,42 +42,58 @@ const CreateProvider = () => {
   } else {
     return (
             <div className="flex flex-col justify-start">
-            <div className="flex justify-around">
+            <div className="flex justify-center">
                 <form className="blog-dash flex flex-col border  rounded-xl mt-2 mb-2" onSubmit={handleSubmit}
                     encType="multipart/form-data">
-                    <h1 className="flex justify-center font-extrabold text-3xl text-primary py-8">Crear un nuevo Proveedor</h1>
-                    <div className=" flex m-8 mb-0">
-                        <label className="bg-quaternary rounded-l-xl w-40 h-8  flex items-center justify-center">Nombre</label>
+                    <h1 className="flex justify-center font-extrabold text-3xl text-primary py-8 pl-2">Crear un nuevo Proveedor</h1>
+                    <div className=" flex m-8 pl-2 mb-0">
+                        <label className="bg-quaternary rounded-l-xl w-32 h-8 pl-2  flex items-center justify-center">Nombre</label>
                         <input
-                            className="bg-formBg rounded-r-lg w-72 h-8"
-                            maxLength="30"
+                            className="bg-formBg rounded-r-lg w-64 h-8 pl-2"
+                            maxLength="25"
                             type='text'
                             name='name'
                             value={inputsForm.name}
                             onChange={handleChange} />
                     </div>
-                    <div className=" flex m-8 mb-0">
-                        <label className="bg-quaternary rounded-l-xl w-40 h-8  flex items-center justify-center">Descuento</label>
+                    <div className="flex my-0 pt-0 pl-8 pl-2 justify-around">
+                      {errors.name
+                        ? <span className="text-warning text-xs font-extrabold py-0 m-0">{errors.name}</span>
+                        : <span className='h-4'></span>}
+                    </div>
+                    <div className=" flex m-8 pl-2 mb-0">
+                        <label className="bg-quaternary rounded-l-xl w-32 h-8 pl-2  flex items-center justify-center">Descuento</label>
                         <input
-                            className="bg-formBg rounded-r-lg w-72 h-8"
+                            className="bg-formBg rounded-r-lg w-44 h-8 pl-2"
                             maxLength="5"
                             type='text'
                             name='discount'
                             value={inputsForm.discount}
                             onChange={handleChange} />
                     </div>
-                    <div className=" flex m-8 mb-0">
-                        <label className="bg-quaternary rounded-l-xl w-40 h-8  flex items-center justify-center">Ganancia</label>
+                    <div className="flex my-0 pt-0 pl-8 pl-2 justify-around">
+                      {errors.discount
+                        ? <span className="text-warning text-xs font-extrabold py-0 m-0">{errors.discount}</span>
+                        : <span className='h-4'></span>}
+                    </div>
+                    <div className=" flex m-8 pl-2 mb-0">
+                        <label className="bg-quaternary rounded-l-xl w-32 h-8 pl-2  flex items-center justify-center">Ganancia</label>
                         <input
-                            className="bg-formBg rounded-r-lg w-72 h-8"
+                            className="bg-formBg rounded-r-lg w-44 h-8 pl-2"
                             maxLength="5"
                             type='text'
                             name='markup'
                             value={inputsForm.markup}
                             onChange={handleChange} />
                     </div>
+                    <div className="flex my-0 pt-0 pl-8 pl-2 justify-around">
+                      {errors.markup
+                        ? <span className="text-warning text-xs font-extrabold py-0 m-0">{errors.markup}</span>
+                        : <span className='h-4'></span>}
+                    </div>
+                    <div className="flex justify-center mt-4">
                     <button
-                        className="rounded-xl w-4/5 h-12 hover:translate-y-1.5 bg-primary text-tertiary border border-solid border-black m-5 font-bold flex items-center justify-center"
+                        className="rounded-xl w-4/5 h-12 hover:translate-y-1.5 bg-primary text-tertiary border border-solid border-black m-5 font-bold "
                         type="submit"
                         disabled={inputsForm.name === '' || inputsForm.discount === '' || inputsForm.markup === ''}
                     >
@@ -87,6 +104,12 @@ const CreateProvider = () => {
                             CREAR PROVEEDOR
                         </h2>
                     </button>
+                    </div>
+                    <div className="flex my-0 pt-0 pl-8 pl-2 justify-around">
+                      {errors.empty
+                        ? <span className="text-warning text-xs font-extrabold py-0 m-0">{errors.empty}</span>
+                        : <span className='h-4'></span>}
+                    </div>
                 </form>
             </div>
         </div>
