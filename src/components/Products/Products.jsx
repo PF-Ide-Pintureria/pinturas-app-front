5efddc88e3077447bc19acdfbf7d50fc4dc1f8ab
Edgar Juan Guercio <jguercio@live.com.ar>
2023-10-03 21:30:26 -0300

    tamaño de img y titulo en las cards

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -10,7 +10,7 @@ const Products = ({ idProduct, name, image, price, prodpackage }) => {
         <h4 className="text-center w-52">{prodpackage}</h4>
         <NavLink
           to={`/products/${idProduct}`}
-          className="relative inline-flex items-center justify-center p-0.5 mt-2 ml-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"
+          className="relative inline-flex items-center justify-center p-0.5 mt-2 ml-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105"
         >
           <p className="relative inline-flex items-center justify-center px-5 py-2.5 transition-all ease-in duration-7 w-52 rounded-md group-hover:bg-opacity-0">
             Ver Producto
