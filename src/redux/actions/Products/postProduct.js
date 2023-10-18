2bb417662a6a9501afd9b732b2b4b2e42d9e8125
Edgar Juan Guercio <jguercio@live.com.ar>
2023-10-18 01:52:42 -0300

    proteccion createProduct

--- src/redux/actions/Products/postProduct.js
+++ src/redux/actions/Products/postProduct.js
@@ -2,10 +2,12 @@ import axios from 'axios'
 import { POST_PRODUCT, BASE_URL } from '../../action-type'
 
 export const postProduct = (productCreate) => {
+  const token = localStorage.getItem('token')
   return async (dispatch) => {
     const response = await axios.post(`${BASE_URL}products`, productCreate, {
       headers: {
-        'Content-Type': 'multipart/form-data'
+        'Content-Type': 'multipart/form-data',
+        Authorization: JSON.parse(token)
       }
     })
 
