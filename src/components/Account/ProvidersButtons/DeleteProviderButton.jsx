d7be086f627b1fa81ab27aa065dd8cac04bd31ae
Edgar Juan Guercio <jguercio@live.com.ar>
2023-10-21 20:09:14 -0300

    proteccion getAllUsers

--- src/components/Account/ProvidersButtons/DeleteProviderButton.jsx
+++ src/components/Account/ProvidersButtons/DeleteProviderButton.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { deleteProvider } from './deleteProvider'
-import { useDispatch } from 'react-redux'
 
 const DeleteProviderButton = ({ providerId }) => {
+
   const handleClick = async (event) => {
     event.preventDefault()
     await deleteProvider(providerId)
   }
+  
   return (
         <div className="justify-start">
             <button onClick={handleClick} className="bg-warning rounded-xl w-20 h-12 text-yellow-300 border-2 border-solid border-gray-300 shadow-md font-bold">Eliminar</button>
--- src/redux/actions/User/getAllUsers.js
+++ src/redux/actions/User/getAllUsers.js
@@ -2,14 +2,18 @@ import { GET_ALL_USERS, BASE_URL } from '../../action-type'
 import axios from 'axios'
 
 export const getAllUsers = () => {
-  return async (dispatch) => {
-    try {
-      const rawResponse = (await axios.get(`${BASE_URL}users/`)).data
-      const response = rawResponse?.users
+    return async (dispatch) => {
+        try {
+            const response = (await axios.get(`${BASE_URL}users/`, {
+                headers: {
+                    Authorization: JSON.parse(localStorage.getItem('token'))
+                }
+            })).data
+            const users = response.users
 
-      return dispatch({ type: GET_ALL_USERS, payload: response })
-    } catch (error) {
-      console.error(error)
+            return dispatch({ type: GET_ALL_USERS, payload: users })
+        } catch (error) {
+            console.error(error.users)
+        }
     }
-  }
 }
