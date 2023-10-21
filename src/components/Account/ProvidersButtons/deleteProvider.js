8f8f8de591ca0c0e62ba28219fc115418682cea3
Edgar Juan Guercio <jguercio@live.com.ar>
2023-10-21 20:32:05 -0300

    espacios ESLINT

--- src/components/Account/ProvidersButtons/deleteProvider.js
+++ src/components/Account/ProvidersButtons/deleteProvider.js
@@ -3,23 +3,23 @@ import { BASE_URL } from '../../../redux/action-type'
 import axios from 'axios'
 
 export const deleteProvider = async (providerId) => {
-    try {
-        await axios.delete(`${BASE_URL}providers/${providerId}`, {
-            headers: {
-                Authorization: JSON.parse(localStorage.getItem('token'))
-            }
-        })
-        Swal.fire({
-            icon: 'success',
-            title: 'Exito!',
-            text: 'El proveedor se eliminó correctamente',
-        })
-    } catch (error) {
-        console.error(error.response)
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Algo falló!',
-        })
-    }
+  try {
+    await axios.delete(`${BASE_URL}providers/${providerId}`, {
+      headers: {
+        Authorization: JSON.parse(localStorage.getItem('token'))
+      }
+    })
+    Swal.fire({
+      icon: 'success',
+      title: 'Exito!',
+      text: 'El proveedor se eliminó correctamente'
+    })
+  } catch (error) {
+    console.error(error.response)
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Algo falló!'
+    })
+  }
 }
--- src/components/ProviderEditForm/formatAndPut.js
+++ src/components/ProviderEditForm/formatAndPut.js
@@ -3,25 +3,25 @@ import { BASE_URL } from '../../redux/action-type'
 import Swal from 'sweetalert2'
 
 export const formatAndPut = async (inputsForm, id) => {
-    try {
-        await axios.put(`${BASE_URL}providers/edit/${id}`, inputsForm, {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: JSON.parse(localStorage.getItem('token'))
-            }
-        })
-        Swal.fire({
-            icon: 'success',
-            text: 'Proveedor editado con exito',
-            confirmButtonText: 'Ok'
-        })
-    } catch (error) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Algo falló!',
-            confirmButtonText: 'Ok'
-        })
-        console.log(error.response.data)
-    }
+  try {
+    await axios.put(`${BASE_URL}providers/edit/${id}`, inputsForm, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: JSON.parse(localStorage.getItem('token'))
+      }
+    })
+    Swal.fire({
+      icon: 'success',
+      text: 'Proveedor editado con exito',
+      confirmButtonText: 'Ok'
+    })
+  } catch (error) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Algo falló!',
+      confirmButtonText: 'Ok'
+    })
+    console.log(error.response.data)
+  }
 }
