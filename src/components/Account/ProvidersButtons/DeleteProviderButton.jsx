c31835b4eeb5d4e2a10d0c39b3c2804906a7041d
Edgar Juan Guercio <jguercio@live.com.ar>
2023-10-21 20:22:07 -0300

    proteccion getAllUsers

--- src/components/Account/ProvidersButtons/DeleteProviderButton.jsx
+++ src/components/Account/ProvidersButtons/DeleteProviderButton.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { deleteProvider } from './deleteProvider'
 
 const DeleteProviderButton = ({ providerId }) => {
-
   const handleClick = async (event) => {
     event.preventDefault()
     await deleteProvider(providerId)
   }
-  
+
   return (
         <div className="justify-start">
             <button onClick={handleClick} className="bg-warning rounded-xl w-20 h-12 text-yellow-300 border-2 border-solid border-gray-300 shadow-md font-bold">Eliminar</button>
