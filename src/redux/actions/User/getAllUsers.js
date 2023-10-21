b50a44a8ff804f277809b23c694f655387d08c3e
Edgar Juan Guercio <jguercio@live.com.ar>
2023-10-21 20:28:10 -0300

    proteccion getAllUsers espacios ESLINT

--- src/redux/actions/User/getAllUsers.js
+++ src/redux/actions/User/getAllUsers.js
@@ -2,18 +2,18 @@ import { GET_ALL_USERS, BASE_URL } from '../../action-type'
 import axios from 'axios'
 
 export const getAllUsers = () => {
-    return async (dispatch) => {
-        try {
-            const response = (await axios.get(`${BASE_URL}users/`, {
-                headers: {
-                    Authorization: JSON.parse(localStorage.getItem('token'))
-                }
-            })).data
-            const users = response.users
-
-            return dispatch({ type: GET_ALL_USERS, payload: users })
-        } catch (error) {
-            console.error(error.response)
+  return async (dispatch) => {
+    try {
+      const response = (await axios.get(`${BASE_URL}users/`, {
+        headers: {
+          Authorization: JSON.parse(localStorage.getItem('token'))
         }
+      })).data
+      const users = response.users
+
+      return dispatch({ type: GET_ALL_USERS, payload: users })
+    } catch (error) {
+      console.error(error.response)
     }
+  }
 }
